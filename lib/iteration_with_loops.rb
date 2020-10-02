8c57062e985429c0c8694fc9d130653da2b6d4a0
Naga Bhargavi Marudhuri <bhargavimarudhuri@gmail.com>
2020-10-02 02:03:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,21 @@
 def join_nested_strings(src)
+  
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
 end
+
+def find_string(src)
+  
+  row_index = 0
+  
+  while row_index < src.count do
+    if src[count].class == string  
+      puts src[0]
+      row_index+=1
+    end 
+  end  
+  
+  
+end
+  find_string(end)
+  
